d1fd768be5bb171c48addeb4085b94694ba8adda
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-10-14 10:31:28 +0200

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by HubertMac on 14/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
